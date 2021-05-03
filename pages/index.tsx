0d66457758461a1a55bf29660a9916ab331b98a9
importamt <dlaqhxoa@gmail.com>
2021-05-03 23:48:01 +0900

    mod - 팝업 표시를 위한 state 및 hero 변경에 대한 내용 반영

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import Intro, {EIntro} from "../components/organisms/Intro/Intro";
+import Hero from "../components/organisms/Hero/Hero";
 import Footer from "../components/organisms/Footer/Footer";
 import Resolution from "../components/organisms/Main/Resolution/Resolution";
 import {ImageGrid} from "../components/organisms/Main";
@@ -6,16 +6,18 @@ import styled from "styled-components";
 import {IImageGridItem} from "../components/molecules/ImageGridItem/ImageGridItem";
 import {useEffect, useState} from "react";
 import Header from "../components/organisms/Header/Header";
+import Popup, {IPopup} from "../components/organisms/Popup/Popup";
 
 
 const MainPage = () => {
 
+    const [selectedPopup, setSelectedPopup] = useState<IPopup>(null)
     const [imageGridItems, setImageGridItems] = useState<IImageGridItem[]>([])
     useEffect(() => {
         const imageGridItems = []
         for (let i = 0; i < 9; i++) {
             imageGridItems.push({
-                image: `/images/main/grid${i + 1}.png`,
+                image: `/images/main/grid${i + 1}.jpg`,
                 text: `Template String ${i + 1}`
             } as IImageGridItem)
         }
@@ -24,10 +26,17 @@ const MainPage = () => {
 
 
     return <SMainPage>
+        {selectedPopup && <Popup {...selectedPopup} handleCloseClick={() => setSelectedPopup(null)}/>}
         <Header/>
-        <Intro type={EIntro.main}/>
+        <Hero
+            image={'/images/main/hero.png'}
+            text={'Creative Design Group!'}
+            height={403}
+        />
         <Resolution/>
-        <ImageGrid column={3} row={3} imageGridItems={imageGridItems}/>
+        <ImageGrid column={3} row={3} imageGridItems={imageGridItems} handleGridItemClick={index => setSelectedPopup({
+            image: `/images/main/popup${index}.jpg`
+        } as IPopup)}/>
         <Footer/>
     </SMainPage>
 }
