a4616e8ad0889cb1f463bec5006b62b8aa5fc7c9
importamt <dlaqhxoa@gmail.com>
2021-05-03 23:47:40 +0900

    mod - about image 추가 ....

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,13 +1,18 @@
 import Header from "../../components/organisms/Header/Header";
-import Intro, {EIntro} from "../../components/organisms/Intro/Intro";
 import Footer from "../../components/organisms/Footer/Footer";
 import styled from "styled-components";
+import Hero from "../../components/organisms/Hero/Hero";
 
 const About = () => {
 
     return <SAbout>
         <Header/>
-        <Intro type={EIntro.about}/>
+        <Hero
+            image={'/images/about/hero.png'}
+            height={788}/>
+        <Hero
+            image={'/images/about/hero2.png'}
+            height={1230}/>
         <Footer/>
     </SAbout>
 }
