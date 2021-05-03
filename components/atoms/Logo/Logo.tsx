a26cec00fc4f329a785fb813a7e31f7de050d900
importamt <dlaqhxoa@gmail.com>
2021-05-03 23:43:38 +0900

    mod - Logo changed img+text to full image without text

--- components/atoms/Logo/Logo.tsx
+++ components/atoms/Logo/Logo.tsx
@@ -1,15 +1,18 @@
 import styled from "styled-components";
+import React from "react";
 
 const logoImage = '/images/common/logo.png'
 const SLogo = styled.a`
   cursor: pointer;
   position: relative;
-  width: 105px;
-  height: 105px;
+  width: 396px;
+  height: 106px;
   background: url(${logoImage}) no-repeat;
   background-size: 100% 100%;
 `
 
-const Logo = () => <SLogo/>
+const Logo = React.forwardRef(
+    (props, ref: any) => <SLogo {...props} ref={ref}/>
+)
 
 export default Logo
