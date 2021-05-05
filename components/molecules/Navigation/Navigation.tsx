5a022fd95aa8494f74f3d09df728008a6060d0dd
importamt <dlaqhxoa@gmail.com>
2021-05-05 22:58:54 +0900

    mod - 메인으로 가던 url 정상화

--- components/molecules/Navigation/Navigation.tsx
+++ components/molecules/Navigation/Navigation.tsx
@@ -4,10 +4,8 @@ import NavigationItem from "../../atoms/NavigationItem/NavigationItem";
 
 const Navigation = () => <SNavigation>
     <NavigationItem url={'/about'}>about</NavigationItem>
-    <NavigationItem url={'/'}>portfolio</NavigationItem>
-    {/*<NavigationItem url={'/portfolio'}>portfolio</NavigationItem>*/}
-    <NavigationItem url={'/'}>contact us</NavigationItem>
-    {/*<NavigationItem url={'/contact-us'}>contact us</NavigationItem>*/}
+    <NavigationItem url={'/portfolio'}>portfolio</NavigationItem>
+    <NavigationItem url={'/contact-us'}>contact us</NavigationItem>
 </SNavigation>
 
 const SNavigation = styled.nav`
