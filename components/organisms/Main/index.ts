8620e7677837507b285f3ef1d6d30cd17cb650e9
importamt <dlaqhxoa@gmail.com>
2021-05-05 22:59:02 +0900

    mod - named export 변경

--- components/organisms/Main/index.ts
+++ components/organisms/Main/index.ts
@@ -1,2 +1,7 @@
 
 export {ImageGrid} from './ImageGrid/ImageGrid'
+export {Clients} from './Clients/Clients'
+export {Resolution} from './Resolution/Resolution'
+export {Fieldset} from './Fieldset/Fieldset'
+export {Agreement} from './Agreement/Agreement'
+export {CompanyInfo} from './CompanyInfo/CompanyInfo'
