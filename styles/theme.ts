36d953d07c8b9acb35869221290e5f34174c4661
importamt <dlaqhxoa@gmail.com>
2021-05-08 23:45:57 +0900

    mod - styled 의 ThemeProvider 를 제대로 사용하는 방법을 모르겠다.

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,31 +1,7 @@
-import {css, CSSProp} from "styled-components";
-
-interface IResponsiveSize {
-    [device: string]: number | number[]
-}
-
-const responsiveSize: IResponsiveSize = {
+const media = {
     mobile: 440,
-    tablet: [440, 1024],
-    desktop: 1024,
-}
-
-interface IMedia {
-    [device: string]: (...styles: string[]) => CSSProp | undefined
-}
-
-const media: IMedia = {
-    mobile: (...styles: string[]) => css`@media only screen and (max-width: ${responsiveSize.mobile}) {
-      ${styles}
-    }`,
-    tablet: (...styles: string[]) =>
-        css`@media only screen and (min-width: ${responsiveSize.tablet[0]})
-        and (max-width: ${responsiveSize.tablet[1]}) {
-          ${styles}
-        }`,
-    desktop: (...styles: string[]) => css`@media only screen and (min-width: ${responsiveSize.desktop}) {
-      ${styles}
-    }`,
+    tablet: 1024,
+    desktop: 1440
 }
 
 const colors = {
@@ -37,7 +13,6 @@ const colors = {
 
 const theme = {
     colors,
-
     media
 }
 
