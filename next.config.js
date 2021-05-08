5df90163e8eb813712e80e5b7d2ce2b76215d3d2
importamt <dlaqhxoa@gmail.com>
2021-05-08 23:46:17 +0900

    add - env 추가
    .env 파일로부터 불러오지만 이것은 공유하지 못한다.

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    env: {
+        KAKAO_KEY: process.env.KAKAO_KEY,
+        EMAILJS_USER_ID: process.env.EMAILJS_USER_ID,
+        EMAILJS_SERVICE_ID: process.env.EMAILJS_SERVICE_ID,
+        EMAILJS_TEMPLATE_ID: process.env.EMAILJS_TEMPLATE_ID,
+    }
+}
