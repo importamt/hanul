292e4e7ea301f3c4c9ef73b6e3c833fb79a047ca
importamt <dlaqhxoa@gmail.com>
2021-05-08 23:42:01 +0900

    mod - Hero 이미지는 어느정도 반응형으로 처리

--- components/organisms/Hero/Hero.tsx
+++ components/organisms/Hero/Hero.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
-import {CSSProperties} from "react";
+import {CSSProperties, useRef} from "react";
+import useResize from "../../../hook/useResize";
 
 interface IHero {
     image: string,
@@ -8,8 +9,19 @@ interface IHero {
     style?: CSSProperties
     children?: any,
 }
-const Hero = (props : IHero) => {
-    return <SHero {...props}>
+
+const Hero = (props: IHero) => {
+    const heroRef = useRef<HTMLDivElement>()
+
+    const handleResize = () => {
+        const hero = heroRef.current
+        const width = props.width || 1920
+        hero.style.height = document.body.clientWidth / width * props.height + 'px'
+    }
+
+    useResize(handleResize)
+
+    return <SHero ref={heroRef} {...props}>
         {props.children}
     </SHero>
 }
@@ -17,14 +29,15 @@ const Hero = (props : IHero) => {
 
 const SHero = styled.section<IHero>`
   position: relative;
-  width: ${({width}) => width?`${width}px`:'100vw'};
+  width: ${({width}) => width ? `${width}px` : '100vw'};
+  min-height: ${({height}) => height / 2}px;
   height: ${({height}) => height}px;
-  
+
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  
+
   background: url(${({image}) => image}) center center no-repeat;
   background-size: cover;
 `
