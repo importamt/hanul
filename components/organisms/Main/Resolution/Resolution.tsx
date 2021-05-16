c0c0f463b29d9f960acfb61adbe01fca42d011a9
importamt <dlaqhxoa@gmail.com>
2021-05-16 11:45:57 +0900

    mod - gray logo added to Resolution
    I thought it was watermark for sample ...

--- components/organisms/Main/Resolution/Resolution.tsx
+++ components/organisms/Main/Resolution/Resolution.tsx
@@ -3,28 +3,47 @@ import styled from "styled-components";
 export const Resolution = ({children}) => <SResolution>
     {children}
 </SResolution>
+
+const grayLogo = '/images/common/gray_logo.png'
 const SResolution = styled.section`
-  height: 300px;
+  width: 100%;
+  min-height: 270px;
   display: flex;
   align-content: center;
   justify-content: center;
   flex-direction: column;
   font-family: 'Noto Sans KR', sans-serif;
+  position: relative;
+  overflow: hidden;
 
   & b {
-
     color: black;
-    font-size: 30px;
-    font-weight: 700;
-    line-height: 50px;
+    font-size: 27px;
+    font-weight: 600;
+    line-height: 60px;
     text-align: center;
   }
 
   & p {
     color: black;
-    font-size: 13px;
-    line-height: 20px;
+    font-size: 15px;
+    line-height: 60px;
     font-weight: 300;
     text-align: center;
+  }
+  
+  &:after {
+    content: '';
+    position: absolute;
+    top: 0;
+    right: 1vw;
+    min-width: 200px;
+    width: 25vw;
+    max-width: 350px;
+    min-height: 200px;
+    height: 25vw;
+    max-height: 350px;
+    background: url(${grayLogo}) no-repeat 0 -5vw;
+    background-size: 100% 100%;
   }
 `
