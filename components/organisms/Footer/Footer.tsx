4104dada0b794fc2f5305f2f6a6dcd824ee3fa30
importamt <dlaqhxoa@gmail.com>
2021-05-16 17:52:48 +0900

    mod - words and font size modified

--- components/organisms/Footer/Footer.tsx
+++ components/organisms/Footer/Footer.tsx
@@ -2,9 +2,9 @@ import styled from "styled-components";
 
 
 const Footer = () => <SFooter>
-    <p>(주)한울기획</p>
+    <strong>한울기획</strong>
     <p>30151 세종특별자치시 시청대로 213 2층 206호(보람동, 금강베네치아) 대표자 : 박정하  |  사업자등록번호 : 889-12-01176  |  전화번호 : 044-212-8391  |  이메일 : hannuul@naver.com</p>
-    <p>COPYRIGHT © hanul. ALL RIGHTS RESERVED</p>
+    <p>COPYRIGHT © hannuul. ALL RIGHTS RESERVED</p>
 </SFooter>
 
 const SFooter = styled.footer`
@@ -18,19 +18,25 @@ const SFooter = styled.footer`
 
   background: ${({theme}) => theme.colors.main};
   
-  & p {
+  & p, & strong {
     color: white;
-    font-size: 1.2vw;
+    font-size: 1.5vw;
     line-height: 20px;
     font-family: 'Noto Sans KR', sans-serif;
     text-align: center;
-
+  }
+  & strong {
+    font-size: 1.8vw;
   }
 
   @media only screen and (min-width: ${({theme}) => theme.media.tablet}px) {
-    & p {
+    & p, & strong {
       font-size: 14px;
       margin: 5px 30px;
+    }
+
+    & strong {
+      font-size: 16px;
     }
   }
 `
