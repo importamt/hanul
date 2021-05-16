c2e355d6d444e73e678b4e4f6c018fd4673c4229
importamt <dlaqhxoa@gmail.com>
2021-05-16 11:46:52 +0900

    mod - hero images changed

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -2,15 +2,19 @@ import Header from "../../components/organisms/Header/Header";
 import Footer from "../../components/organisms/Footer/Footer";
 import styled from "styled-components";
 import Hero from "../../components/organisms/Hero/Hero";
-import {Clients} from "../../components/organisms/Main";
+import {Clients, Resolution} from "../../components/organisms/Main";
 
 const About = () => {
 
     return <SAbout>
         <Header/>
         <Hero
-            image={'/images/about/hero.png'}
-            height={788}/>
+            image={'/images/about/hero.jpg'}
+            height={403}/>
+        <Resolution>
+            <b>CORE VALUE</b>
+            <p>소통 / 열정 / 센스 / 신뢰</p>
+        </Resolution>
         <Hero
             image={'/images/about/hero2.png'}
             height={1230}/>
